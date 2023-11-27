006f1b6f59669602bfdf58be982fd4ea7ae05f8c
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2023-11-27 20:04:09 +0100

    feat: add missing translation for menu

---
+++ items-shop/src/components/ui/UiButton/UiButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="flex items-center cursor-pointer justify-center px-5 py-4 rounded btn primary btn--rounded">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  &.primary {
+    @apply bg-shopPrimary text-shopGray-50 hover:brightness-75
+  }
+
+  &--rounded {
+    @apply rounded-full px-11
+  }
+}
+
+</style>
--- items-shop/src/pages/index.vue
+++ items-shop/src/pages/index.vue
@@ -3,14 +3,24 @@
 import useGetApiLink from '~/composables/useGetApiLink';
 import type {Product} from "../../../types/Product";
 import type {BaseApiListResponse} from "../../../types/BaseApiListResponse";
+import UiButton from "~/components/ui/UiButton/UiButton.vue";
 
 const products = ref<Product[]>([]);
 
-  const {data} = await useFetch<BaseApiListResponse<Product>>(useGetApiLink('products'));
+const {data} = await useFetch<BaseApiListResponse<Product>>(useGetApiLink('products'));
 
 </script>
 <template>
   <div>
-    <NuxtImg format="avif,webp" src="Logo.png" sizes="xs:30vw sm:40vw md:50vw" ></NuxtImg>
+
+    <UiButton>
+      Search
+    </UiButton>
+
+    <div></div>
+    <div></div>
+    <div></div>
+
+    <NuxtImg format="avif,webp" src="Logo.png" sizes="xs:30vw sm:40vw md:50vw"></NuxtImg>
   </div>
 </template>
